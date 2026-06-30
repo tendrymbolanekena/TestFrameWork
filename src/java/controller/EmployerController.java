d6a9778f8a41eb5c88bbbe84d339eb28057b47da
tendrymbolanekena <tendrymbolanekena@gmail.com>
2026-06-30 08:28:09 +0300

    cxvbnmk,

--- src/java/controller/EmployerController.java
+++ src/java/controller/EmployerController.java
@@ -1,12 +1,31 @@
 package controller;
 
 import annotation.Controller;
-import annotation.RequestMapping;
+import annotation.UrlMapping;
 
 @Controller
 public class EmployerController {
-    @RequestMapping("/test/qwertq")
+    @UrlMapping(value="/li")
     public void liste() {
         System.out.println("test");
     }
+
+    @UrlMapping("/delete")
+    public void delete() {
+        System.out.println("test");
+    }
+
+    @UrlMapping("/update")
+    public void update() {
+        System.out.println("test");
+    }
+
+    @UrlMapping("/andrana")
+    public void andrana() {
+        System.out.println("test");
+    }
+
+
+
+
 }
